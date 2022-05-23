1e583e1d355e2d9e7e7189b2fc0a97168ae0f431
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-23 20:55:13 -0300

    feat(prisma) create module & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { GamesModule } from './games/games.module';
 import { GendersModule } from './genders/genders.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [GamesModule, GendersModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
