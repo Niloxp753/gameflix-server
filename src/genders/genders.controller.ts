76eee40bdb1d5f0b9ffefc2e59085ec846e63d0a
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-22 23:14:41 -0300

    feat(genders): add `create` endpoint

--- src/genders/genders.controller.ts
+++ src/genders/genders.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('genders')
 export class GendersController {
   @Get()
   findAll() {
     return 'Buscar todos os gêneros';
+  }
+
+  @Post()
+  create() {
+    return 'Criar um gênero';
   }
 }
