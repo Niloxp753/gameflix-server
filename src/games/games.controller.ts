f4bfc139e34bacb003c81b9562fe5f2d2b65c6c8
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-22 21:30:09 -0300

    feat(games): add `create` endpoint

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('games')
 export class GamesController {
   @Get()
   findAll() {
     return 'Buscar todos os jogos';
+  }
+
+  @Post()
+  create() {
+    return 'Criar um jogo';
   }
 }
