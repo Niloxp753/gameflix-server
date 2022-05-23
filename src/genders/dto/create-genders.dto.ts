ed7a62d2fbf1d705683abcc6bc332e12fcb4bf31
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-22 23:56:03 -0300

    docs: implement swagger genders

--- src/genders/dto/create-genders.dto.ts
+++ src/genders/dto/create-genders.dto.ts
@@ -1,3 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
 export class CreateGendersDto {
+  @IsString()
+  @ApiProperty({
+    description: 'O nome do gênero',
+    example: 'Ação',
+  })
   name: string;
 }
--- src/genders/genders.controller.ts
+++ src/genders/genders.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateGendersDto } from './dto/create-genders.dto';
 import { GendersService } from './genders.service';
-
+@ApiTags('genders')
 @Controller('genders')
 export class GendersController {
   constructor(private gendersService: GendersService) {}
