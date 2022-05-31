6930866f6c9d5cf7991ddf988ccd8bd0e7a598ad
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-30 21:58:16 -0300

    feat(profile): implement dto validation

--- src/profiles/dto/create-profile.dto.ts
+++ src/profiles/dto/create-profile.dto.ts
@@ -1,4 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl } from 'class-validator';
+
 export class CreateProfileDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Título do perfil',
+    example: 'Kids',
+  })
   title: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do perfil',
+    example:
+      'https://w7.pngwing.com/pngs/750/504/png-transparent-children-s-games-cartoon-children-playing-game-child-reading-thumbnail.png',
+  })
   imageURL: string;
 }
