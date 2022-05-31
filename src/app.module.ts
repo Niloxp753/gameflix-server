f9fe978caefe314ac885996452a296094cd0f436
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-30 22:09:54 -0300

    feat(users): generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,16 @@ import { GendersModule } from './genders/genders.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ProfilesModule } from './profiles/profiles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [GamesModule, GendersModule, PrismaModule, ProfilesModule],
+  imports: [
+    GamesModule,
+    GendersModule,
+    PrismaModule,
+    ProfilesModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
