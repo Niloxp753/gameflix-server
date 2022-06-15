fdb4caaef961c53e2ba993c06ef89aec17731483
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-06-14 22:10:26 -0300

    feat: implement setup from deploy

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3444);
+  await app.listen(process.env.PORT || 3444);
 }
 bootstrap();
